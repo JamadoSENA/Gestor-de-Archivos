0a1e7f8483b51c8a751e80d5628a24e1e1a64b5c
JulianAmado <j.amadosena@gmail.com>
2023-11-13 11:14:08 -0500

    Clase 8. Adelantos

--- vistas/header.php
+++ vistas/header.php
@@ -3,15 +3,17 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Gestor</title>
-        <link rel="stylesheet" href="../librerias/bootstrap4/bootstrap.min..css">
+        <link rel="shortcut icon" href="../img/Login/HeadLogo.png" type="image/x-icon">
+        <title>Gestor de archivos de Juliancito</title>
+        <link rel="stylesheet" href="../librerias/bootstrap4/bootstrap.min.css">
+        <link rel="stylesheet" href="../librerias/fontawesome/css/all.css">
     </head>
     <body>
         <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
     <div class="container">
         <a class="navbar-brand" href="#">
-        <img src="https://placeholder.pics/svg/150x50/888888/EEE/Logo" alt="..." height="36">
+        <img src="../img/logo_buildoc_blanco.png" alt="..." height="36">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -19,23 +21,17 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="inicio.php"> <span class="fas fa-house"></span> Inicio</a>
+            <span class="sr-only">(current)</span>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="gestor.php"> <span class="fas fa-folder"></span> Administrar</a>
             </li>
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-            </a>
-            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li>
-                <hr class="dropdown-divider">
-                </li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+            <li class="nav-item">
+            <a class="nav-link" href="#"> <span class="fas fa-file"></span> Reportes</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#"> <span class="fas fa-power-off"></span> Salir</a>
             </li>
         </ul>
         </div>
